0e625d4b7156580989734e34084d7c6c345dc976
tomas rada <Tomas.rada.024@pslib.cz>
2025-05-19 21:32:28 +0200

    Aktualizovat main.ts

--- main.ts
+++ main.ts
@@ -1,71 +1,80 @@
 
 radio.setGroup(234)
 radio.setFrequencyBand(54)
-control.deviceSerialNumber()
 radio.setTransmitSerialNumber(true)
 
 
 
+
+
+
+const hodnoty = []
 let x: number = 0
 let y: number = 0
 let x_l: number = 0
 let y_l: number = 0
 // Hodnoty souřadnic z gyroskopu
 //to do
+input.onButtonPressed(Button.A, function() {
+    radio.sendValue("njimok@<>", control.deviceSerialNumber() )
+})
+basic.forever(function () {
+    x =input.acceleration(Dimension.X)
+    y =input.acceleration(Dimension.Y)
 
 
-x =input.acceleration(Dimension.X)
-y =input.acceleration(Dimension.Y)
+    // Kontrola osy X
+    if (x > 100) {
+        x_l = 3
+
+    } 
+    else if (x > 60) {
+        x_l= 2
+
+    } 
+    else if (x > 30) {
+        x_l = 1
+
+    }
+    //to do 
+    else if (x > 0 && x < 0) {
+        x_l = 0
+    } 
+    else if (x < -100) {
+        x_l = -3
+    } 
+    else if (x < -60) {
+        x_l = -2
+    } 
+    else if (x < -30) {
+        x_l = -1
+    }
 
 
-// Kontrola osy X
-if (x > 100) {
-    x_l = 3
+    // Kontrola osy Y
+    if (y > 100) {
+        x_l = 3
+    }
+    else if (y > 60) {
+        x_l = 2
+    }
+    else if (y > 30) {
+        x_l = 1 
+    }
+    //to do
+    else if (y > 0 && y<0) {
+        x_l = 0
+    }
+    else if (y < -100) {
+        x_l = -3
+    }
+    else if (y < -60) {
+        x_l = -2
+    }
+    else if (y < -30) {
+        x_l = -1
+    }
+    hodnoty.push([x_l, y_l])
+    basic.pause(20)
 
-} 
-else if (x > 60) {
-    x_l= 2
-
-} 
-else if (x > 30) {
-    x_l = 1
-
-}
-//to do 
-else if (x > 0 && x < 0) {
-    x_l = 0
-} 
-else if (x < -100) {
-    x_l = -3
-} 
-else if (x < -60) {
-    x_l = -2
-} 
-else if (x < -30) {
-    x_l = -1
-}
-
-
-// Kontrola osy Y
-if (y > 100) {
-    x_l = 3
-}
-else if (y > 60) {
-    x_l = 2
-}
-else if (y > 30) {
-    x_l = 1 
-}
-//to do
-else if (y > 0 && y<0) {
-    x_l = 0
-}
-else if (y < -100) {
-    x_l = -3
-}
-else if (y < -60) {
-    x_l = -2
-}
-else if (y < -30) {
-    x_l = -1
-}
+})
